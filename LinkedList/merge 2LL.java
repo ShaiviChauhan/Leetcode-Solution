88d2e584cae8b618ef4aabe12fd0cee3fbbdca16
Shaivi Chauhan <85447046+ShaiviChauhan@users.noreply.github.com>
2022-08-31 16:50:22 +0530

    Update merge 2LL.java

--- LinkedList/merge 2LL.java
+++ LinkedList/merge 2LL.java
@@ -1,40 +1,21 @@
-class LinkedList
-{
-    //Function to merge two sorted linked list.
-    Node sortedMerge(Node head1, Node head2) {
-     // This is a "method-only" submission. 
-     // You only need to complete this method
-     
-      Node head3;
-        if(head1 == null){
-            return head2;
-        }
-        if(head2 == null){
-            return head1;
-        }
-        if(head1.data<= head2.data){
-            head3 = head1;
-            head1 = head1.next; // Move to the Next Node
-            
-        }
-        else{
-            head3 = head2;
-            head2 = head2.next;
-        }
-        Node temp = head3;
-        while(head1!=null && head2!=null){
-            if(head1.data<=head2.data){
-                temp.next = head1;
-                temp = temp.next;
-                head1 = head1.next; 
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy= new ListNode(0);
+        ListNode curr=dummy;
+        
+        while(l1!=null && l2!=null){
+            //merge list
+            if(l1.val<=l2.val){
+                curr.next=l1;
+                l1=l1.next;    
             }
             else{
-                temp.next = head2;
-                temp = temp.next;
-                head2 = head2.next; 
+                curr.next=l2;
+                l2=l2.next;
             }
+            curr=curr.next;
         }
-        temp.next = head1!=null?head1: head2;
-        return head3;
-   } 
+        curr.next=l1!=null?l1:l2;
+            return dummy.next;
+    }
 }
